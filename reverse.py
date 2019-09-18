f876e05c3db0d10ee6a227117089f74fec124b07
Kaja <catcalledkaja@gmail.com>
2019-09-18 19:43:30 +0200

    learning algorithms

---
+++ reverse.py
@@ -0,0 +1,13 @@
+
+print([1,2,3].reverse)
+
+arrays = [1,2,3]
+distance = [] 
+for x in arrays: 
+    distance.append( x[-1] x[0])
+    print(distance)
+
+    x[-1]..x[0]
+
+for x in arrays:
+    distance.append(x[y] > x[y])
