b739a651115683c140a37446db9e167188b67d41
Kaja <catcalledkaja@gmail.com>
2019-09-30 13:31:40 +0100

    adding reverse?

--- reverse.py
+++ reverse.py
@@ -1,13 +1,1 @@
-
-print([1,2,3].reverse)
-
-arrays = [1,2,3]
-distance = [] 
-for x in arrays: 
-    distance.append( x[-1] x[0])
-    print(distance)
-
-    x[-1]..x[0]
-
-for x in arrays:
-    distance.append(x[y] > x[y])
+print("hello"[::-1]) #[begin:end:step]
